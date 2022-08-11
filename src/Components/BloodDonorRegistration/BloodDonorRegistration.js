a9d1236d728599f49c6c3aea631a6831d6cb4a42
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-08-12 02:53:51 +0600

    navbar problem fixed

--- src/Components/BloodDonorRegistration/BloodDonorRegistration.js
+++ src/Components/BloodDonorRegistration/BloodDonorRegistration.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Header from '../Header/Header';
 import './BloodDonorRegistration.css'
+import donorImg from '../../Resources/blood-img-1.png'
 
 const BloodDonorRegistration = () => {
     return (
-        <div className='donor-registration-bg h-screen'>
+        <div className='donor-registration-bg'>
             <Header></Header>
+
+            <div class="hero min-h-screen">
+                <div class="hero-content flex-col lg:flex-row">
+                    <img src={donorImg} class="max-w-lg rounded-lg shadow-2xl hidden lg:block" />
+                    <div>
+                        <h1 class="text-5xl font-bold my-font text-white tracking-wide">আপনার তথ্য দিন</h1>
+                        <p class="py-6">sdfsdfsdfsdfsdffsdfsfsdfsfsdfds</p>
+                        <button class="btn">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -12,11 +12,11 @@ const Header = () => {
                 <input type="checkbox" id='click' />
                 <label for="click" className='menu-btn'> <FaStream /> </label>
                 <ul>
-                    <li className=''><HashLink to="/"> <span className='nav-link active'>হোম</span></HashLink></li>
-                    <li><HashLink to="/donorRegistration"> <span className='nav-link'>রেজিষ্ট্রেশন করুন</span> </HashLink></li>
+                    <li className=''><HashLink to="/"> <span className='nav-link active text-center custom-width'>হোম</span></HashLink></li>
+                    <li><HashLink to="/donorRegistration"> <span className='nav-link active'>রেজিষ্ট্রেশন করুন</span> </HashLink></li>
                     <li><HashLink to="/"> <span className='nav-link'>যোগাযোগ করুন</span> </HashLink></li>
                     <li><HashLink to="/"> <span className='nav-link'>আমাদের সম্পর্কে</span> </HashLink></li>
-                    <li><HashLink to="/"> <span className='nav-link'>লগইন</span> </HashLink></li>
+                    <li><HashLink to="/"> <span className='nav-link text-center custom-width'>লগইন</span> </HashLink></li>
                 </ul>
             </nav>
         </div>
