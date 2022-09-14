52bb84f834e6f6d3c1191b1c5b05f49bf7b2f4a7
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-09-15 03:47:22 +0600

    recent blood donation activity done

--- src/Components/DashboardGroup/Analytics/Activity/RecentBloodDonation.js
+++ src/Components/DashboardGroup/Analytics/Activity/RecentBloodDonation.js
@@ -1,9 +1,44 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
 import { BiDonateHeart } from "react-icons/bi";
+import { useQuery } from 'react-query';
+import { useNavigate } from 'react-router-dom';
+import auth from '../../../../firebase.init';
+import Loading from '../../../Shared/Loading/Loading';
 const RecentBloodDonation = () => {
+
+    const navigate = useNavigate()
+    const { data: recentCompleteDonation, isLoading, refetch } = useQuery('recentCompleteDonation', () => fetch('http://localhost:5000/complete-blood-request', {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    })
+        .then(res => {
+            if (res.status === 401 || res.status === 403) {
+                signOut(auth)
+                localStorage.removeItem('accessToken')
+                navigate('/')
+            }
+            return res.json()
+        }))
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    const latest5Donation = recentCompleteDonation.slice(0, 5)
+    console.log(latest5Donation);
+
     return (
         <div className='border border-gray-300 rounded-xl p-4 mt-5 w-lg shadow-lg'>
             <p className='text-[#141C39] text-xl poppins-font font-semibold mb-4 flex items-center'> <span className='mr-3 text-red-500 text-2xl'><BiDonateHeart/> </span>Recent Blood Donation</p>
+
+            {
+                latest5Donation?.map(singleDonation => <div className='grid grid-cols-3 border-b py-1.5'> <p className='bangla-font'>{singleDonation.patient_name}</p> <p className='bangla-font text-center'>{singleDonation.blood_quantity
+                } ব্যাগ</p> <p className='capitalize poppins-font text-red-500 font-bold text-center'>{singleDonation.requested_bloodGroup}</p> </div> )
+            }
         </div>
     );
 };
--- src/Components/DashboardGroup/Analytics/Activity/RecentDonor.js
+++ src/Components/DashboardGroup/Analytics/Activity/RecentDonor.js
@@ -9,7 +9,7 @@ import { useQuery } from 'react-query';
 const RecentDonor = () => {
 
     const navigate = useNavigate()
-    const { data: allDonorList, isLoading, refetch } = useQuery('donorList', () => fetch('http://localhost:5000/verified-donor', {
+    const { data: recentDonor, isLoading, refetch } = useQuery('recentDonor', () => fetch('http://localhost:5000/verified-donor', {
         method: 'GET',
         headers: {
             'content-type': 'application/json',
@@ -29,15 +29,14 @@ const RecentDonor = () => {
         return <Loading />
     }
 
-    const latest5Donor = allDonorList.slice(0, 5)
-    console.log(latest5Donor);
+    const latest5Donor = recentDonor.slice(0, 5)
 
     return (
         <div className='border border-gray-300 rounded-xl p-4 mt-5 w-lg shadow-lg'>
             <p className='text-[#141C39] text-xl poppins-font font-semibold mb-4 flex items-center'> <span className='mr-3 text-red-500'><ImFire/> </span> New Donors</p>
 
             {
-                latest5Donor?.map(newDonor => <div className='grid grid-cols-3 border-b py-1.5'> <p className='bangla-font'>{newDonor.name}</p> <p className='bangla-font text-center'>{newDonor.gender}</p> <p className='capitalize poppins-font text-red-500 font-bold text-center'>{newDonor.bloodGroup}</p> </div> )
+                latest5Donor?.map(singleDonor => <div className='grid grid-cols-3 border-b py-1.5'> <p className='bangla-font'>{singleDonor.name}</p> <p className='bangla-font text-center'>{singleDonor.gender}</p> <p className='capitalize poppins-font text-red-500 font-bold text-center'>{singleDonor.bloodGroup}</p> </div> )
             }
         </div>
     );
