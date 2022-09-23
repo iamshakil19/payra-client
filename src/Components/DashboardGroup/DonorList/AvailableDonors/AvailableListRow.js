9e7ee30711ca5db44af5f9a7ecb804b5b1d860e5
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-09-24 01:15:20 +0600

    trying to fix live server problem

--- src/Components/DashboardGroup/DonorList/AvailableDonors/AvailableListRow.js
+++ src/Components/DashboardGroup/DonorList/AvailableDonors/AvailableListRow.js
@@ -6,12 +6,12 @@ const AvailableListRow = ({ donorSingleData, index, setAvailableDonorData, setAv
     const { donationCount, name, profileImg, age, gender, number1, bloodGroup, policeStation, union, village, _id } = donorSingleData
 
     const myDonationDate = '09-24-2022 01:03:00'
-    let donateTime = myDonationDate.toLocaleDateString();
+    // let donateTime = myDonationDate.toLocaleDateString();
 
 
     const handleDonate = () => {
         var date = new Date(new Date().getTime() + (90 * 24 * 60 * 60 * 1000));
-        // let donateTime = date.toLocaleDateString();
+        let donateTime = date.toLocaleDateString();
         console.log(donateTime);
 
         const bloodRequestStatusInfo = {
